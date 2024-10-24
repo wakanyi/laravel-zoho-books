35c8dd487be578d70bcd488c5d111f6c118dfa84
Sumer Ahmed <sumer5020@gmail.com>
2024-10-24 17:44:57 +0300

    Chore: Test list contact added

--- tests/Unit/ContactUnitTest.php
+++ tests/Unit/ContactUnitTest.php
@@ -3,6 +3,8 @@
 namespace Sumer5020\ZohoBooks\Tests\Unit;
 
 use Sumer5020\ZohoBooks\DTOs\Arguments\ContactDto;
+use Sumer5020\ZohoBooks\DTOs\PaginationDto;
+use Sumer5020\ZohoBooks\DTOs\QueryParameters\ContactFiltersQpDto;
 use Sumer5020\ZohoBooks\Facades\ZohoBooksFacade;
 use Sumer5020\ZohoBooks\Tests\TestCase;
 
@@ -22,12 +24,34 @@ class ContactUnitTest extends TestCase
         $this->assertEquals(0, $response['code']);
     }
 
+    /**
+     * @return void
+     */
     public function testUpdateContact(){
         $accessToken = ZohoBooksFacade::authentications()->createAccessToken();
         $organization_id = '';
         $contactDto = new ContactDto([]);
 
         $response = ZohoBooksFacade::contacts()->create($accessToken, $organization_id, $contactDto);
+
+        $this->assertIsArray($response);
+        $this->assertEquals(0, $response['code']);
+    }
+
+    /**
+     * @return void
+     */
+    public function testListContact(){
+        $accessToken = ZohoBooksFacade::authentications()->createAccessToken();
+        $organization_id = '';
+
+        $paginationDto = new PaginationDto([
+            'page' => 1,
+            'per_page' => 10
+        ]);
+        $contactFiltersDto = new ContactFiltersQpDto([]);
+
+        $response = ZohoBooksFacade::contacts()->list($accessToken, $organization_id, $paginationDto, $contactFiltersDto);
 
         $this->assertIsArray($response);
         $this->assertEquals(0, $response['code']);
